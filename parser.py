984884cabfc64cf93c276d4839f5212984efe2c2
gkarthik <xraydiffraction@gmail.com>
2020-03-11 22:26:46 -0700

    Replace None with ""

--- parser.py
+++ parser.py
@@ -163,6 +163,9 @@ def load_annotations(data_folder):
                 item["deaths"] = deaths.loc[ind, row.index[4 + date_ind]]
                 annotations[_id] = item
     for _id, annt in annotations.items():
-        for i in ["confirmed", "recovered", "deaths"]:
-            annt[i] = int(annt[i])
+        for k,v in annt:
+            if k in ["confirmed", "recovered", "deaths"]:
+                annt[i] = int(annt[i])
+            if v == None:
+                annt[k] = ""
         yield {"_id": _id, "annotations": annt}
