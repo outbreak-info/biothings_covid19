4aca920ed16658bc175240ed71054215259bf3e0
gkarthik <xraydiffraction@gmail.com>
2020-03-15 01:42:16 -0700

    Modified parser to aggregate at admin0 and admin1

--- parser.py
+++ parser.py
@@ -63,6 +63,96 @@ def get_closest_polygon(shp, lat, lng):
             min_dist = dist
     return closest_feat
 
+def aggregate_countries(orig_df, shp, feats = []):
+    df = orig_df.copy()
+    countries = []
+    print("Computing country spatial joins")
+    df["feats"] = df[["Lat", "Long"]].apply(lambda x: get_closest_polygon(shp, x["Lat"], x["Long"]), axis = 1) if len(feats) == 0 else feats
+    print("Completed  spatial joins")
+    df["computed_country"] = df["feats"].apply(lambda i: i["properties"]["ADM0_A3"])
+    for n, grp in df.groupby("computed_country"):
+        states = grp[~grp["Province/State"].isna()]
+        country = grp[grp["Province/State"].isna()]
+        admin0_feature = grp.iloc[0]["feats"]
+        # Lat Long
+        grp_lat_lng = grp.iloc[0][grp.columns[2:4]]
+        if states.shape[0] > 0 and country.shape[0] > 0:
+            print("{}: State and country level available. Manual check required to sum.".format(n))
+        if states.shape[0] > 0 and country.shape[0] == 0:
+            print("{}: Only states. Summing all".format(n))
+        if states.shape[0] == 0 and country.shape[0] > 0:
+            print("{}: Only country".format(n))
+        grp_sum = grp.sum()[grp.columns[4:-2]]
+        grp_lat_lng.append(grp_sum)
+        row = grp_lat_lng.append(grp_sum)
+        row["JHU_Lat"] = row["Lat"]
+        row["JHU_Long"] = row["Long"]
+        row["admin_level"] = 0
+        row["name"] = admin0_feature["properties"]["NAME"]
+        row["iso3"] = admin0_feature["properties"]["ADM0_A3"]
+        row["location_id"] = n
+        row["population"] = admin0_feature["properties"]["POP_EST"]
+        geom = admin0_feature["geometry"]
+        centroid = get_centroid(geom)
+        row["lat"] = centroid[1]
+        row["long"] = centroid[0]
+        countries.append(row)
+    countries_df = pd.concat(countries, axis = 1).transpose()
+    return countries_df, df["feats"].tolist()
+
+def aggregate_states(orig_df, admn0_shp, admn1_shp, feats = []):
+    df = orig_df.copy()
+    df_states = df[~df["Province/State"].isna()]
+    df_states["feats"] = df_states[["Lat", "Long"]].apply(lambda x: get_closest_polygon(admn1_shp, x["Lat"], x["Long"]), axis = 1) if len(feats) == 0 else feats
+    df_states["computed_state"] = df_states["feats"].apply(lambda i: i["properties"]["iso_3166_2"]+ "_" + i["properties"]["adm0_a3"])
+    states = []
+    for n, grp in df_states.groupby("computed_state"):
+        grp_lat_lng = grp.iloc[0][grp.columns[2:4]]
+        grp_sum = grp.sum()[grp.columns[4:-2]]
+        admin1_feature = grp.iloc[0]["feats"]
+        row = grp_lat_lng.append(grp_sum)
+        row["JHU_Lat"] = row["Lat"]
+        row["JHU_Long"] = row["Long"]
+        row["admin_level"] = 1
+        row["name"] = admin1_feature["properties"]["name"]
+        row["iso3"] = admin1_feature["properties"]["iso_3166_2"]
+        row["location_id"] = n
+        row["country_iso3"] = admin1_feature["properties"]["adm0_a3"]
+        row["country_name"] = admin1_feature["properties"]["admin"]
+        country_pop = [i["properties"]["POP_EST"] for i in admn0_shp if i["properties"]["ADM0_A3"] == row["country_iso3"]][0]
+        row["country_population"] = country_pop
+        geom = admin1_feature["geometry"]
+        centroid = get_centroid(geom)
+        row["lat"] = centroid[1]
+        row["long"] = centroid[0]
+        print("Finished computing for {}".format(n))
+        states.append(row)
+    states_df = pd.concat(states, axis = 1).transpose()
+    return states_df, df_states["feats"].tolist()
+
+def get_stats(confirmed_row, dead_row, recovered_row, date_cols):
+    confirmed_row = confirmed_row[date_cols]
+    recovered_row = recovered_row[date_cols]
+    dead_row = dead_row[date_cols]
+    attr = {}
+    first = {}
+    to_date = lambda x: dt.strptime(x, "%m/%d/%y").strftime("%Y-%m-%d")
+    for row, n in zip([confirmed_row, dead_row, recovered_row], ["confirmed", "dead", "recovered"]):
+        attr[n+"_currentToday"] = to_date(row.index[-1])
+        attr[n+"_numIncrease"] = row[-1] - row[-2]
+        attr[n+"_pctIncrease"] = (row[-1] - row[-2])/row[-2] if row[-2] > 0 else 0
+        diff = row[row.diff() > 0]
+        first_date = to_date(diff.index[0]) if diff.shape[0] > 0 else ""
+        first[n] = first_date
+        attr[n+"_firstDate"] = first_date
+        attr[n+"_newToday"] = True if row.index[-1] == first_date else False
+        attr[n+"_currentCases"] = row[-1]
+    if first["confirmed"] != "" and first["dead"] != "":
+        attr["first_dead-first_confirmed"] = (dt.strptime(first["confirmed"], "%Y-%m-%d") - dt.strptime(first["dead"], "%Y-%m-%d")).days
+    else:
+        attr["first_dead-first_confirmed"] = ""
+    return attr
+
 def load_annotations(data_folder):
     annotations = {}
     # Read shapefiles
@@ -84,89 +174,47 @@ def load_annotations(data_folder):
     deaths = deaths[deaths["Province/State"].apply(lambda x: "princess" not in x.lower() if not pd.isna(x) else True)]
     recovered = recovered[recovered["Province/State"].apply(lambda x: "princess" not in x.lower() if not pd.isna(x) else True)]
 
-    for ind, row in confirmed.iterrows():
-        lat = 0
-        lng = 0
-        attr = {
-            "admin_level": "",
-            "admin0": "",
-            "admin1": "",
-            "admin2": "",
-            "admin0_iso3": "",
-            "admin1_iso3": "",
-            "admin2_fips": "",
-            "admin0_pop": "",
-            "lat": "",
-            "lng": "",
-            "date": ""
-        }
-        m = []
-        geom = None
-        admin_level = None
-        admin0_feature = None
-        admin1_feature = None
-        admin2_feature = None
-        # Get country either way
-        admin0_feature = get_closest_polygon(admn0_shp, row["Lat"], row["Long"])
-        if pd.isna(row["Province/State"]):  # Case count set at country level only
-            admin_level = 0
-        else:
-            admin1_feature = get_closest_polygon(admn1_shp, row["Lat"], row["Long"])
-            admin_level = 1
-            if "County" in row["Province/State"] or ", " in row["Province/State"]:
-                admin2_feature = get_closest_polygon(usa_admn2_shp, row["Lat"], row["Long"])
-                admin_level = 2
-        if admin0_feature == None:
-            logging.warning("Country not found for indice {} with name {}".format(ind, row["Country/Region"]))
-            continue
-        attr["admin_level"] = admin_level
-        attr["admin0"] = admin0_feature["properties"]["NAME"]
-        attr["admin0_iso3"] = admin0_feature["properties"]["ADM0_A3"]
-        attr["admin0_pop"] = admin0_feature["properties"]["POP_EST"]
-        geom = admin0_feature["geometry"]
-        if admin_level > 0:         # For admin_level 1 and 2
-            if admin1_feature == None:
-                logging.warning("State not found for indice {} with name {}".format(ind, row["State/Province"]))
-                continue
-            attr["admin1"] = admin1_feature["properties"]["name"]
-            attr["admin1_iso3"] = admin1_feature["properties"]["iso_3166_2"]
-            geom = admin1_feature["geometry"]
-            if admin_level == 2:
-                if admin2_feature == None:
-                    logging.warning("County not found for indice {} with name {}".format(ind, row["State/Province"]))
-                    continue
-                attr["admin2"] = admin2_feature["properties"]["NAME"]
-                attr["admin2_fips"] = admin2_feature["properties"]["STATEFP"] + admin2_feature["properties"]["COUNTYFP"]
-                geom = admin2_feature["geometry"]
-        attr["lng"], attr["lat"]  = get_centroid(geom)
-        attr["old_lat"] = row["Lat"]
-        attr["old_lng"] = row["Long"]
-        attr["old_name"] = "{}, {}".format(row["Province/State"], row["Country/Region"])
-        if ind % 20 == 0:
-            logging.info("Completed {} records".format(ind + 1))
-        get_text = lambda x: x if x != None else ""
-        for date_ind, d in enumerate([dt.strptime(i, "%m/%d/%y").strftime("%Y-%m-%d") for i in row.index[4:]]):
-            # ID includes admin0_iso3 + admin1_iso3 + admin2_fips + date
-            id_text = get_text(attr["admin0_iso3"]) + get_text(attr["admin1_iso3"]) + get_text(attr["admin2_fips"]) + get_text(attr["admin2"]) + d
-            hash_id = hashlib.md5(id_text.encode())
-            _id = hash_id.hexdigest()
-            if _id in annotations:
-                annotations[_id]["confirmed"] += row[row.index[4 + date_ind]]
-                annotations[_id]["recovered"] += recovered.loc[ind, row.index[4 + date_ind]]
-                annotations[_id]["deaths"] += deaths.loc[ind, row.index[4 + date_ind]]
-            else:
-                item = copy.deepcopy(attr)
-                item["date"] = d
-                item["id_text"] = id_text
-                item["confirmed"] = row[row.index[4 + date_ind]]
-                item["recovered"] = recovered.loc[ind, row.index[4 + date_ind]]
-                item["deaths"] = deaths.loc[ind, row.index[4 + date_ind]]
-                annotations[_id] = item
-    for _id, annt in annotations.items():
-        for k,v in annt.items():
-            if k in ["confirmed", "recovered", "deaths"]:
-                annt[k] = int(v)
-            if v == None:
-                annt[k] = ""
-        annt["_id"] = _id
-        yield annt
+    countries_confirmed, feats = aggregate_countries(confirmed, admn0_shp)
+    countries_recovered, feats = aggregate_countries(recovered, admn0_shp, feats)
+    countries_dead, feats = aggregate_countries(deaths, admn0_shp, feats)
+
+    states_confirmed, feats = aggregate_states(confirmed, admn0_shp, admn1_shp)
+    states_recovered, feats = aggregate_states(recovered, admn0_shp, admn1_shp, feats)
+    states_dead, feats = aggregate_states(deaths, admn0_shp, admn1_shp, feats)
+
+    items = []
+    for (cid, conf), (rid, recov), (did, dead) in zip(countries_confirmed.iterrows(), countries_recovered.iterrows(), countries_dead.iterrows()):
+        date_cols = countries_confirmed.columns[2:-9]
+        item = {}
+        for i in countries_confirmed.columns[-9:]:
+            item[i] = conf[i]
+        for d in date_cols:
+            ditem = copy.deepcopy(item)
+            ditem["confirmed"] = conf[d]
+            ditem["recovered"] = recov[d]
+            ditem["dead"] = dead[d]
+            ditem["date"] = dt.strptime(d, "%m/%d/%y").strftime("%Y-%m-%d")
+            attr = get_stats(conf, recov, dead, date_cols)
+            for k,v in attr.items():
+                ditem[k] = v
+            items.append(ditem)
+
+    for (cid, conf), (rid, recov), (did, dead) in zip(states_confirmed.iterrows(), states_recovered.iterrows(), states_dead.iterrows()):
+        date_cols = states_confirmed.columns[2:-11]
+        item = {}
+        for i in states_confirmed.columns[-11:]:
+            item[i] = conf[i]
+        for d in date_cols:
+            ditem = copy.deepcopy(item)
+            ditem["confirmed"] = conf[d]
+            ditem["recovered"] = recov[d]
+            ditem["dead"] = dead[d]
+            ditem["date"] = dt.strptime(d, "%m/%d/%y").strftime("%Y-%m-%d")
+            attr = get_stats(conf, recov, dead, date_cols)
+            for k,v in attr.items():
+                ditem[k] = v
+            items.append(ditem)
+
+    for item in items:
+        item["_id"] = item["location_id"]+"_"+item["date"]
+        yield item
