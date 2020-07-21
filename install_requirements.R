dd494b468474b5adfbb0bc98407a52fa4ce0f652
gkarthik <xraydiffraction@gmail.com>
2020-07-21 11:29:07 -0700

    Added repo explicitly

--- install_requirements.R
+++ install_requirements.R
@@ -1,1 +1,1 @@
-install.packages(c("readr", "dplyr", "tidyr", "stringr", "classInt", "jsonlite", "sf", "ggplot2", "gganimate", "magick", "data.table"))
+install.packages(c("readr", "dplyr", "tidyr", "stringr", "classInt", "jsonlite", "sf", "ggplot2", "gganimate", "magick", "data.table"), repos="http://cran.us.r-project.org")
