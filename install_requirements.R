9120bf98fdcb3a81268bd26f82092e85db44295d
gkarthik <xraydiffraction@gmail.com>
2020-08-02 16:27:02 -0700

    Added XML from another repo for R v3.6.3

--- install_requirements.R
+++ install_requirements.R
@@ -1,1 +1,2 @@
-install.packages(c("readr", "dplyr", "tidyr", "stringr", "classInt", "jsonlite", "sf", "ggplot2", "gganimate", "magick", "data.table"), repos="http://cran.us.r-project.org", dependencies = TRUE)
+install.packages("XML", repos = "http://www.omegahat.net/R")
+install.packages(c("readr", "dtplyr", "dplyr", "tidyr", "stringr", "classInt", "jsonlite", "sf", "ggplot2", "gganimate", "magick", "data.table"), repos="http://cran.us.r-project.org", dependencies = TRUE)
