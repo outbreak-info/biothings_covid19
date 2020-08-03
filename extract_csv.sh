63bec4480a05079b36fed17774e857e7006c6aa0
gkarthik <xraydiffraction@gmail.com>
2020-08-03 16:18:55 -0700

    Add logs statements to stderr

--- extract_csv.sh
+++ extract_csv.sh
@@ -5,20 +5,20 @@ jsonout=$(awk -F "=" '/out_json_path/ {print $2}' config.ini)
 
 mkdir -p $gifdata
 
-echo "Reading json from ${jsonout}"
+echo "Reading json from ${jsonout}" 1>&2
 
 jq -r '["date","location_id","iso3","state_iso3","name","admin_level", "confirmed_per_100k", "confirmed_rolling", "confirmed_rolling_per_100k", "confirmed_rolling_14days_ago_diff", "confirmed_rolling_14days_ago_diff_per_100k", "dead_per_100k", "dead_rolling", "dead_rolling_per_100k", "dead_rolling_14days_ago_diff", "dead_rolling_14days_ago_diff_per_100k"], (.[] | select(.admin_level==2 and .country_iso3=="USA" ) | [.date,.location_id,.iso3,.state_iso3,.name,.admin_level,.confirmed_per_100k, .confirmed_rolling, .confirmed_rolling_per_100k, .confirmed_rolling_14days_ago_diff, .confirmed_rolling_14days_ago_diff_per_100k, .dead_per_100k, .dead_rolling, .dead_rolling_per_100k, .dead_rolling_14days_ago_diff, .dead_rolling_14days_ago_diff_per_100k]) | @csv' $jsonout > ${gifdata}test_counties.csv
 
-echo "Wrote counties csv to ${gifdata}test_counties.csv"
+echo "Wrote counties csv to ${gifdata}test_counties.csv" 1>&2
 
 jq -r '["date","location_id","iso3","name","admin_level", "confirmed_per_100k", "confirmed_rolling", "confirmed_rolling_per_100k", "confirmed_rolling_14days_ago_diff", "confirmed_rolling_14days_ago_diff_per_100k", "dead_per_100k", "dead_rolling", "dead_rolling_per_100k", "dead_rolling_14days_ago_diff", "dead_rolling_14days_ago_diff_per_100k"], (.[] | select(.admin_level==1 and .country_iso3=="USA" ) | [.date,.location_id,.iso3,.name,.admin_level,.confirmed_per_100k, .confirmed_rolling, .confirmed_rolling_per_100k, .confirmed_rolling_14days_ago_diff, .confirmed_rolling_14days_ago_diff_per_100k, .dead_per_100k, .dead_rolling, .dead_rolling_per_100k, .dead_rolling_14days_ago_diff, .dead_rolling_14days_ago_diff_per_100k]) | @csv' $jsonout > ${gifdata}test_states.csv
 
-echo "Wrote states csv to ${gifdata}test_states.csv"
+echo "Wrote states csv to ${gifdata}test_states.csv" 1>&2
 
 jq -r '["date","location_id","iso3","name","admin_level", "confirmed_per_100k", "confirmed_rolling", "confirmed_rolling_per_100k", "confirmed_rolling_14days_ago_diff", "confirmed_rolling_14days_ago_diff_per_100k", "dead_per_100k", "dead_rolling", "dead_rolling_per_100k", "dead_rolling_14days_ago_diff", "dead_rolling_14days_ago_diff_per_100k"], (.[] | select(.admin_level==1.5 and .country_name=="United States of America") | [.date,.location_id,.iso3,.name,.admin_level,.confirmed_per_100k, .confirmed_rolling, .confirmed_rolling_per_100k, .confirmed_rolling_14days_ago_diff, .confirmed_rolling_14days_ago_diff_per_100k, .dead_per_100k, .dead_rolling, .dead_rolling_per_100k, .dead_rolling_14days_ago_diff, .dead_rolling_14days_ago_diff_per_100k]) | @csv' $jsonout > ${gifdata}test_metros.csv
 
-echo "Wrote metro csv to ${gifdata}test_metros.csv"
+echo "Wrote metro csv to ${gifdata}test_metros.csv" 1>&2
 
 jq -r '["date","location_id","iso3","name","admin_level", "confirmed_per_100k", "confirmed_rolling", "confirmed_rolling_per_100k", "confirmed_rolling_14days_ago_diff", "confirmed_rolling_14days_ago_diff_per_100k", "dead_per_100k", "dead_rolling", "dead_rolling_per_100k", "dead_rolling_14days_ago_diff", "dead_rolling_14days_ago_diff_per_100k"], (.[] | select(.admin_level==0) | [.date,.location_id,.iso3,.name,.admin_level,.confirmed_per_100k, .confirmed_rolling, .confirmed_rolling_per_100k, .confirmed_rolling_14days_ago_diff, .confirmed_rolling_14days_ago_diff_per_100k, .dead_per_100k, .dead_rolling, .dead_rolling_per_100k, .dead_rolling_14days_ago_diff, .dead_rolling_14days_ago_diff_per_100k]) | @csv' $jsonout  > ${gifdata}test_admin0.csv
 
-echo "Wrote country csv to ${gifdata}test_admin0.csv"
+echo "Wrote country csv to ${gifdata}test_admin0.csv" 1>&2
