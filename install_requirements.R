ecf6ad350bc54bf2be0a16547c00147d00c2abdf
Emory Hufbauer <mindoftea@gmail.com>
2021-07-15 09:16:24 -0700

    ini package install

--- install_requirements.R
+++ install_requirements.R
@@ -1,2 +1,3 @@
 install.packages("XML", repos = "http://www.omegahat.net/R")
 install.packages(c("readr", "dtplyr", "dplyr", "tidyr", "stringr", "classInt", "jsonlite", "sf", "ggplot2", "gganimate", "magick", "data.table", "optparse"), repos="http://cran.us.r-project.org", dependencies = TRUE)
+install.packages("ini", repos="http://cran.us.r-project.org", dependencies = TRUE)
