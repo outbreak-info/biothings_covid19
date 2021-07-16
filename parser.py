8a7ca65dbd3bccf85b1a52daeaca3100d6c66920
Julia Mullen <julia.l.mullen@gmail.com>
2021-07-16 12:39:39 -0400

    jsonl change for biothings parser

--- parser.py
+++ parser.py
@@ -7,17 +7,15 @@ from biothings import config
 logging = config.logger
 
 def load_annotations(data_folder):
-    json_path = os.path.join(data_folder,"biothings_items.json.gz")
-    items = []
+    json_path = os.path.join(data_folder,"biothings_items.jsonl.gz")
     with gzip.open(json_path) as f:
-        json_bytes = f.read()
-        json_str = json_bytes.decode("utf-8")
-        items = json.loads(json_str)
-        f.close()
-    for item in items:
-        for k,v in item.items():
-            if type(v) == np.int64:
-                item[k] = int(v)
-            if type(v) == np.float64 or type(v) == np.float:
-                item[k] = float(v)
-        yield item
+        for line in f:
+            item = json.loads(line)
+            
+            for k,v in item.items():
+                if type(v) == np.int64:
+                    item[k] = int(v)
+                if type(v) == np.float64 or type(v) == np.float:
+                    item[k] = float(v)
+
+            yield item
