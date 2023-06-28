ea8c83a68a6d0dce1e4de3aef7c9c56d09f3b15d
Everaldo <everaldorodrigo@gmail.com>
2023-06-27 19:21:23 -0700

    Refactor parser.py

--- parser.py
+++ parser.py
@@ -1,21 +1,15 @@
 import os
 import json
 import gzip
-import numpy as np
 
-from biothings import config
-logging = config.logger
+from biothings.utils.dataload import to_number
 
 def load_annotations(data_folder):
     json_path = os.path.join(data_folder,"epi_data.jsonl.gz")
     with gzip.open(json_path) as f:
         for line in f:
+            # TODO: use biothings.utils.serializer import load_json when upgrade biothings.api
             item = json.loads(line)
-            
             for k,v in item.items():
-                if type(v) == int:
-                    item[k] = int(v)
-                if type(v) == float:
-                    item[k] = float(v)
-
+                item[k] = to_number(v)
             yield item
